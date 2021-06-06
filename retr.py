9e5417216c2210dac37ee41215dd868f32218c67
Benjamín Mario Sainz-Tinajero <bm.sainz@gmail.com>
2021-06-06 09:06:43 -0500

    Update retr.py

--- retr.py
+++ retr.py
@@ -26,20 +26,10 @@ def retrieval(data):
         X = np.array(list(csv.reader(open('data/glass_X.csv', newline=''))))
         y = list(csv.reader(open('data/glass_y.csv', newline='')))
         X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
-    elif data == 'transfusion':
-        n_clusters = 2
-        X = np.array(list(csv.reader(open('data/transfusion_X.csv', newline=''))))
-        y = list(csv.reader(open('data/transfusion_y.csv', newline='', encoding='utf-8-sig')))
-        X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
     elif data == 'forest':
         n_clusters = 4
         X = np.array(list(csv.reader(open('data/forest_X.csv', newline='', encoding='utf-8-sig'))))
         y = list(csv.reader(open('data/forest_y.csv', newline='', encoding='utf-8-sig')))
-        X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
-    elif data == 'spambase':
-        n_clusters = 2
-        X = np.array(list(csv.reader(open('data/spambase_X.csv', newline='', encoding='utf-8-sig'))))
-        y = list(csv.reader(open('data/spambase_y.csv', newline='', encoding='utf-8-sig')))
         X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
     elif data == 'segment':
         n_clusters = 7
@@ -51,20 +41,10 @@ def retrieval(data):
         X = np.array(list(csv.reader(open('data/breast-tissue_X.csv', newline='', encoding='utf-8-sig'))))
         y = list(csv.reader(open('data/breast-tissue_y.csv', newline='', encoding='utf-8-sig')))
         X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
-    elif data == 'knowledge':
-        n_clusters = 4
-        X = np.array(list(csv.reader(open('data/knowledge_X.csv', newline='', encoding='utf-8-sig'))))
-        y = list(csv.reader(open('data/knowledge_y.csv', newline='', encoding='utf-8-sig')))
-        X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
     elif data == 'leaf':
         n_clusters = 36
         X = np.array(list(csv.reader(open('data/leaf_X.csv', newline='', encoding='utf-8-sig'))))
         y = list(csv.reader(open('data/leaf_y.csv', newline='', encoding='utf-8-sig')))
-        X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
-    elif data == 'congress':
-        n_clusters = 2
-        X = np.array(list(csv.reader(open('data/congress_X.csv', newline='', encoding='utf-8-sig'))))
-        y = list(csv.reader(open('data/congress_y.csv', newline='', encoding='utf-8-sig')))
         X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
     elif data == 'dermatology':
         n_clusters = 6
@@ -73,27 +53,10 @@ def retrieval(data):
         X = np.array(X)
         y = list(csv.reader(open('data/dermatology_y.csv', newline='', encoding='utf-8-sig')))
         X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
-    elif data == 'yeast':
-        n_clusters = 10
-        X = np.array(list(csv.reader(open('data/yeast_X.csv', newline='', encoding='utf-8-sig'))))
-        X = pd.DataFrame(X)
-        X = np.array(X.loc[:, 1:])
-        y = list(csv.reader(open('data/yeast_y.csv', newline='', encoding='utf-8-sig')))
-        X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
     elif data == 'zoo':
         n_clusters = 7
         X = np.array(list(csv.reader(open('data/zoo_X.csv', newline='', encoding='utf-8-sig'))))
         y = list(csv.reader(open('data/zoo_y.csv', newline='', encoding='utf-8-sig')))
-        X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
-    elif data == 'ionosphere':
-        n_clusters = 2
-        X = np.array(list(csv.reader(open('data/ionosphere_X.csv', newline='', encoding='utf-8-sig'))))
-        y = list(csv.reader(open('data/ionosphere_y.csv', newline='', encoding='utf-8-sig')))
-        X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
-    elif data == 'heart':
-        n_clusters = 2
-        X = np.array(list(csv.reader(open('data/heart_X.csv', newline='', encoding='utf-8-sig'))))
-        y = list(csv.reader(open('data/heart_y.csv', newline='', encoding='utf-8-sig')))
         X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
     elif data == 'liver':
         n_clusters = 16
@@ -118,11 +81,6 @@ def retrieval(data):
         X = np.array(list(csv.reader(open('data/jain_X.csv', newline='', encoding='utf-8-sig'))))
         y = list(csv.reader(open('data/jain_y.csv', newline='', encoding='utf-8-sig')))
         X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
-    elif data == 'titanic':
-        n_clusters = 2
-        X = np.array(list(csv.reader(open('data/titanic_X.csv', newline='', encoding='utf-8-sig'))))
-        y = list(csv.reader(open('data/titanic_y.csv', newline='', encoding='utf-8-sig')))
-        X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
     elif data == 'parkinsons':
         n_clusters = 2
         X = np.array(list(csv.reader(open('data/parkinsons_X.csv', newline='', encoding='utf-8-sig'))))
@@ -132,16 +90,6 @@ def retrieval(data):
         n_clusters = 3
         X = np.array(list(csv.reader(open('data/seeds_X.csv', newline='', encoding='utf-8-sig'))))
         y = list(csv.reader(open('data/seeds_y.csv', newline='', encoding='utf-8-sig')))
-        X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
-    elif data == 'urban-land-cover':
-        n_clusters = 9
-        X = np.array(list(csv.reader(open('data/urban-land-cover_X.csv', newline='', encoding='utf-8-sig'))))
-        y = list(csv.reader(open('data/urban-land-cover_y.csv', newline='', encoding='utf-8-sig')))
-        X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
-    elif data == 'wine-quality-red':
-        n_clusters = 6
-        X = np.array(list(csv.reader(open('data/wine-quality-red_X.csv', newline='', encoding='utf-8-sig'))))
-        y = list(csv.reader(open('data/wine-quality-red_y.csv', newline='', encoding='utf-8-sig')))
         X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
     elif data == 'pathbased':
         n_clusters = 3
@@ -153,11 +101,4 @@ def retrieval(data):
         X = np.array(list(csv.reader(open('data/aggregation_X.csv', newline='', encoding='utf-8-sig'))))
         y = list(csv.reader(open('data/aggregation_y.csv', newline='', encoding='utf-8-sig')))
         X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
-    elif data == 'wholesale':
-        n_clusters = 2
-        X = np.array(list(csv.reader(open('data/wholesale_X.csv', newline='', encoding='utf-8-sig'))))
-        y = list(csv.reader(open('data/wholesale_y.csv', newline='', encoding='utf-8-sig')))
-        X, y = X.astype(float), np.array([item for sublist in y for item in sublist])
     return data, n_clusters, X, y
-
-# data, n_clusters, X, y = retrieval('wine')
