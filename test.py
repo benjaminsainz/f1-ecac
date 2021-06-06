8a9e689f48db7c627b97ad045203b476c9e5b741
Benjamín Mario Sainz-Tinajero <bm.sainz@gmail.com>
2021-06-06 09:10:15 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,15 +6,13 @@ Héctor Gibrán Ceballos-Cancino, Francisco Javier Cantu-Ortiz
 from gen import *
 from retr import *
 
-full = ['breast-tissue', 'dermatology', 'ecoli', 'forest', 'glass', 'iris', 'leaf', 'liver', 'transfusion', 'wine',
-        'knowledge', 'segment', 'congress', 'zoo', 'heart', 'ionosphere', 'r15', 'spiral', 'breast-cancer-wisconsin',
-        'jain', 'titanic', 'parkinsons', 'seeds', 'urban-land-cover', 'pathbased', 'aggregation', 'wholesale']
-
-
 def test(ds=full, pop_size=200, max_gens=200, runs=1):
     for d in ds:
         data, n_clusters, X, y = retrieval(d)
         f1ecac_run(X, n_clusters, data, pop_size, max_gens, runs=runs, y=y, log_file=False, evolutionary_plot=False)
 
+full = ['aggregation', 'breast-cancer-wisconsin', 'breast-tissue', 'dermatology', 'ecoli', 'forest', 'glass', 'iris',
+       'jain', 'leaf', 'liver', 'parkinsons', 'pathbased', 'r15', 'seeds', 'segment', 'spiral', 'transfusion', 'wine',
+       'zoo']
 
-test(['zoo'])
+test(full)
